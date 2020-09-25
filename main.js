bf946638f3ebfbde2ec1374b6dc89c18b5831598
Danish <skdanish1999@gmail.com>
2020-09-26 00:09:20 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const DaysEl = document.getElementById('days');
+const HoursEl = document.getElementById('hours');
+const MinutesEl = document.getElementById('minutes');
+const SecondsEl = document.getElementById('seconds');
+
+
+const birthDay = '1 Dec 2020';
+
+function countDown(){
+	const birthDayDate = new Date(birthDay);
+	const currentDate = new Date();
+	const totalSeconds =Math.floor( (birthDayDate - currentDate) / 1000);
+
+	const days = Math.floor(totalSeconds / 3600 / 24);
+	const hours = Math.floor((totalSeconds / 3600) %24 );
+	const minutes = Math.floor((totalSeconds / 60) % 60);
+	const seconds = Math.floor((totalSeconds) % 60);
+	// console.log(days, hours, minutes, seconds);
+
+	DaysEl.innerHTML = format(days);
+	HoursEl.innerHTML = format(hours);
+	MinutesEl.innerHTML = format(minutes);
+	SecondsEl.innerHTML = format(seconds);
+}
+
+function format(time){
+	return time < 10 ? `0${time}` : time;
+}
+
+countDown(); 
+
+setInterval(countDown,1000);
